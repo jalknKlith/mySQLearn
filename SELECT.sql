9e5b79493ea6084d165cf10af6c6749e3a5ecd7c
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-20 11:20:42 -0500

    Update SELECT.sql

--- SELECT.sql
+++ SELECT.sql
@@ -4,5 +4,20 @@ SELECT * FROM tableName WHERE columnName='Value';
 'select all the records from a tableName where the value of the columnName starts with an "a"'
 SELECT * FROM tableName WHERE columnName LIKE 'a%';
 
-to return only different values
+--to return only different values
 SELECT DISTINCT * FROM tableName;
+
+-- to search DB
+SELECT DBname;
+
+-- to order by
+SELECT colNAME FROM tableNAME order by colNAME ASC|DESC;
+
+-- to test for null values
+SELECT colNAME FROM tableNAME WHERE colNAME IS NULL;
+SELECT colNAME FROM tableNAME WHERE colNAME IS NOT NULL;
+
+-- to limit
+SELECT * FROM tableNAME LIMIT number;
+SELECT * FROM tableNAME LIMIT number OFFSET number;
+SELECT colNAME FROM tableNAME WHERE colNAME = 'Value' LIMIT number;
