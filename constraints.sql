2b750d59bf70868e46672356c3c68d6b7efa773a
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-22 12:50:20 -0500

    Create constraints.sql

---
+++ constraints.sql
@@ -0,0 +1,7 @@
+NOT NULL      Ensures that a column cannot have a NULL value
+UNIQUE        Ensures that all values in a column are different
+PRIMARY KEY   A combination of a NOT NULL and UNIQUE. Uniquely identifies each row in a table
+FOREIGN KEY   Prevents actions that would destroy links between tables
+CHECK         Ensures that the values in a column satisfies a specific condition
+DEFAULT       Sets a default value for a column if no value is specified
+CREATE INDEX  Used to create and retrieve data from the database very quickly
