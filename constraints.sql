c956aebdf6d90762eba70618c82a59f512f48e43
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-22 13:01:52 -0500

    Update constraints.sql

--- constraints.sql
+++ constraints.sql
@@ -5,3 +5,70 @@ FOREIGN KEY   Prevents actions that would destroy links between tables
 CHECK         Ensures that the values in a column satisfies a specific condition
 DEFAULT       Sets a default value for a column if no value is specified
 CREATE INDEX  Used to create and retrieve data from the database very quickly
+
+-- example NOT NULL
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255) NOT NULL,
+    Age int
+);
+
+ALTER TABLE Persons
+MODIFY Age int NOT NULL;
+
+-- example UNIQUE
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    UNIQUE (ID)
+);
+
+ALTER TABLE Persons
+ADD UNIQUE (ID);
+
+-- To name a UNIQUE constraint, and to define a UNIQUE constraint on multiple columns
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CONSTRAINT UC_Person UNIQUE (ID,LastName)
+);
+
+ALTER TABLE Persons
+ADD CONSTRAINT UC_Person UNIQUE (ID,LastName);
+
+-- To drop a UNIQUE constraint
+ALTER TABLE Persons
+DROP INDEX UC_Person;
+
+-- example PRIMARY KEY
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    PRIMARY KEY (ID)
+);
+
+ALTER TABLE Persons
+ADD PRIMARY KEY (ID);
+
+-- to define a PRIMARY KEY constraint on multiple columns
+CREATE TABLE Persons (
+    ID int NOT NULL,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
+);
+
+ALTER TABLE Persons
+ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
+
+-- to drop a PRIMARY KEY
+ALTER TABLE Persons
+DROP PRIMARY KEY;
