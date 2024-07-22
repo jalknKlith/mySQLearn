ad79b6a97c0b3747e5516ed5b41848220b98c5b1
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-22 12:48:49 -0500

    Update creaTable.sql

--- creaTable.sql
+++ creaTable.sql
@@ -5,7 +5,6 @@ CREATE TABLE tableName (
 );
 
 -- EXAMPLE
-
 CREATE TABLE Persons (
     PersonID int,
     LastName varchar(255),
@@ -13,3 +12,25 @@ CREATE TABLE Persons (
     Address varchar(255),
     City varchar(255)
 );
+
+-- TO CREATE FROM ANOTHER TABLE
+CREATE TABLE newTableName AS
+    SELECT col1, col2,...
+    FROM existingTableName
+    WHERE condition;
+
+-- DELETE TABLE
+DROP TABLE tableName;
+
+-- to delete the data inside a table, but not the table itself.
+TRUNCATE TABLE tableName;
+
+-- to add, delete, or modify columns in an existing table
+ALTER TABLE tableName
+ADD columnName datatype;
+
+ALTER TABLE tableName
+DROP COLUMN columnName;
+
+ALTER TABLE table_name
+MODIFY COLUMN columnName datatype;
