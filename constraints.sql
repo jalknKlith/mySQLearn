c8bd799dbedbcfb2deb7252f054ae655fd54c72f
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-22 14:05:05 -0500

    Update constraints.sql

--- constraints.sql
+++ constraints.sql
@@ -29,7 +29,7 @@ CREATE TABLE Persons (
 ALTER TABLE Persons
 ADD UNIQUE (ID);
 
--- To name a UNIQUE constraint, and to define a UNIQUE constraint on multiple columns
+-- to name a UNIQUE constraint, and to define a UNIQUE constraint on multiple columns
 CREATE TABLE Persons (
     ID int NOT NULL,
     LastName varchar(255) NOT NULL,
@@ -41,65 +41,7 @@ CREATE TABLE Persons (
 ALTER TABLE Persons
 ADD CONSTRAINT UC_Person UNIQUE (ID,LastName);
 
--- To drop a UNIQUE constraint
+-- to drop a UNIQUE constraint
 ALTER TABLE Persons
 DROP INDEX UC_Person;
-
--- example PRIMARY KEY
-CREATE TABLE Persons (
-    ID int NOT NULL,
-    LastName varchar(255) NOT NULL,
-    FirstName varchar(255),
-    Age int,
-    PRIMARY KEY (ID)
-);
-
-ALTER TABLE Persons
-ADD PRIMARY KEY (ID);
-
--- to define a PRIMARY KEY constraint on multiple columns
-CREATE TABLE Persons (
-    ID int NOT NULL,
-    LastName varchar(255) NOT NULL,
-    FirstName varchar(255),
-    Age int,
-    CONSTRAINT PK_Person PRIMARY KEY (ID,LastName)
-);
-
-ALTER TABLE Persons
-ADD CONSTRAINT PK_Person PRIMARY KEY (ID,LastName);
-
--- to drop a PRIMARY KEY
-ALTER TABLE Persons
-DROP PRIMARY KEY;
-
--- FOREIGN KEY
-CREATE TABLE Orders (
-    OrderID int NOT NULL,
-    OrderNumber int NOT NULL,
-    PersonID int,
-    PRIMARY KEY (OrderID),
-    FOREIGN KEY (PersonID) REFERENCES Persons(PersonID)
-);
-
-ALTER TABLE Orders
-ADD FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
-
-ALTER TABLE Orders
-DROP FOREIGN KEY FK_PersonOrder;
-
--- defining a FOREIGN KEY constraint on multiple columns
-CREATE TABLE Orders (
-    OrderID int NOT NULL,
-    OrderNumber int NOT NULL,
-    PersonID int,
-    PRIMARY KEY (OrderID),
-    CONSTRAINT FK_PersonOrder FOREIGN KEY (PersonID)
-    REFERENCES Persons(PersonID)
-);
-
-ALTER TABLE Orders
-ADD CONSTRAINT FK_PersonOrder
-FOREIGN KEY (PersonID) REFERENCES Persons(PersonID);
-
 
