f4806792c2a2c889478380bb64c108cef3324851
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-22 12:41:50 -0500

    Update creaTable.sql

--- creaTable.sql
+++ creaTable.sql
@@ -3,3 +3,13 @@ CREATE TABLE tableName (
     colName2 datatype,
     colName3 datatype
 );
+
+-- EXAMPLE
+
+CREATE TABLE Persons (
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255)
+);
