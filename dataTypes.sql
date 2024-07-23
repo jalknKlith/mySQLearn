92fba8d6f76e515091198c8071a56eb9f197472c
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-23 18:57:55 -0500

    Update dataTypes.sql

--- dataTypes.sql
+++ dataTypes.sql
@@ -78,3 +78,19 @@ DOUBLE PRECISION(size, d)
 
 DECIMAL(size, d), DEC(size, d) Equal to DECIMAL(size,d)
 An exact fixed-point number. The total number of digits is specified in size. The number of digits after the decimal point is specified in the d parameter. The maximum number for size is 65. The maximum number for d is 30. The default value for size is 10. The default value for d is 0.
+
+-- DATE AND TIME
+DATE
+A date. Format: YYYY-MM-DD. The supported range is from '1000-01-01' to '9999-12-31'
+
+DATETIME(fsp)
+A date and time combination. Format: YYYY-MM-DD hh:mm:ss. The supported range is from '1000-01-01 00:00:00' to '9999-12-31 23:59:59'. Adding DEFAULT and ON UPDATE in the column definition to get automatic initialization and updating to the current date and time
+
+TIMESTAMP(fsp)
+A timestamp. TIMESTAMP values are stored as the number of seconds since the Unix epoch ('1970-01-01 00:00:00' UTC). Format: YYYY-MM-DD hh:mm:ss. The supported range is from '1970-01-01 00:00:01' UTC to '2038-01-09 03:14:07' UTC. Automatic initialization and updating to the current date and time can be specified using DEFAULT CURRENT_TIMESTAMP and ON UPDATE CURRENT_TIMESTAMP in the column definition
+
+TIME(fsp)
+A time. Format: hh:mm:ss. The supported range is from '-838:59:59' to '838:59:59'
+
+YEAR
+A year in four-digit format. Values allowed in four-digit format: 1901 to 2155, and 0000.
