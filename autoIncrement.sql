92cc6027db8e1d45e8739538dd215158ec939524
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-23 18:34:16 -0500

    Create autoIncrement.sql

---
+++ autoIncrement.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Persons (
+    Personid int NOT NULL AUTO_INCREMENT,
+    LastName varchar(255) NOT NULL,
+    FirstName varchar(255),
+    Age int,
+    PRIMARY KEY (Personid)
+);
+
+ALTER TABLE tableName AUTO_INCREMENT;
