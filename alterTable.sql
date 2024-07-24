bfbdaa2378c8d29688911bcadf102c4105c43a70
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-24 08:40:12 -0500

    Create alterTable.sql

---
+++ alterTable.sql
@@ -0,0 +1,9 @@
+-- to add, delete, or modify columns in an existing table.
+
+-- adding a column
+ALTER TABLE tableName
+ADD columnName datatype;
+
+-- dropping a column
+ALTER TABLE table_name
+DROP COLUMN column_name;
