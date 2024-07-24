5ecef7c734fa7f1ae6f28c3591289fe9ec0d6fdf
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-23 19:21:38 -0500

    Update insertINTO.sql

--- insertINTO.sql
+++ insertINTO.sql
@@ -1,4 +1,8 @@
-INSERT INTO tableName (col1, col2, col3) VALUES 
+INSERT INTO tableName (col1, col2, col3)
+VALUES 
 ('value1', 'value2', 'value3'),
 ('value1', 'value2', 'value3'),
 ('value1', 'value2', 'value3');
+
+INSERT INTO tableName
+VALUES ('value1', 'value2', 'value3');
