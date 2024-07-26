d521cec1d499ad11a0e1c080c79abf4973ab94e6
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-25 23:07:12 -0500

    Create DELETE.sql

---
+++ DELETE.sql
@@ -0,0 +1,7 @@
+-- delete all the records from a table 
+DELETE FROM tableName;
+
+-- delete all the records from a table where the column has a certain value
+DELETE FROM tableName
+WHERE colName = 'Value';
+
