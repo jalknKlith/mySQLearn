648204c7be3d5bc1f330213b353165a1bd41cd61
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-25 23:03:33 -0500

    Update UPDATE.sql

--- UPDATE.sql
+++ UPDATE.sql
@@ -1,3 +1,14 @@
 -- updating a column of all records in a table
 UPDATE tableName
 SET colName = "Value";
+
+-- updating a column value where is a certain value
+UPDATE tableName
+SET colName = "Value"
+WHERE colName = "new value";
+
+-- updating two or more columns values, where is a specific value in another column
+UPDATE tableName
+SET colName = 'Value',
+colName2 = 'Value'
+WHERE colName3 = "Value";
