53dc18c0257f6176d4bcc622f4d4ff9a77bf02b3
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-25 22:35:26 -0500

    Create ORDER_BY.sql

---
+++ ORDER_BY.sql
@@ -0,0 +1,12 @@
+-- searching all records from a table, sorting the result alphabetically
+SELECT * FROM tableName
+ORDER BY colName;
+
+-- searching all records from a table, sorting the result reversed alphabetically
+SELECT * FROM tableName
+ORDER BY colName DESC;
+
+-- searching all records from a table, sorting the result alphabetically
+-- by two or more columns
+SELECT * FROM tableName
+ORDER BY colName1, colName2;
