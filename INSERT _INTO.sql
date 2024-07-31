13db38f4fac557990f44da55cf3ec42db01b94d3
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-07-31 10:25:32 -0500

    Update INSERT _INTO.sql

--- INSERT _INTO.sql
+++ INSERT _INTO.sql
@@ -7,23 +7,34 @@ VALUES
 INSERT INTO tableName
 VALUES ('value1', 'value2', 'value3');
 
--- updating a column of all records in a table
+--updating a column of all records in a table
 UPDATE tableName
 SET colName = "Value";
 
--- updating a column value where is a certain value
+--updating a column value where is a certain value
 UPDATE tableName
 SET colName = "Value"
 WHERE colName = "new value";
 
--- updating two or more columns values, where is a specific value in another column
+--updating two or more columns values, where is a specific value in another column
 UPDATE tableName
 SET colName = 'Value',
 colName2 = 'Value'
 WHERE colName3 = "Value";
 
--- to delete all the data inside a table
+--to delete all the data inside a table
 TRUNCATE TABLE Persons;
+
+--copying data from one table and inserts it into another table
+INSERT INTO table1
+SELECT * FROM table2
+WHERE condition;
+
+--copying only some columns from one table into another table:
+INSERT INTO table2 (column1, column2, column3, ...)
+SELECT column1, column2, column3, ...
+FROM table1
+WHERE condition;
 
 -- making an ALIAS of the column name
 SELECT tableName AS aliasName
