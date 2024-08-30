2797b0bfd5118e400e2503509d64cd9d50bd8825
jalknKleith <169000561+jalknKleith@users.noreply.github.com>
2024-08-30 08:32:26 -0500

    Update expImport.sql

--- expImport.sql
+++ expImport.sql
@@ -1,3 +1,22 @@
+-- Upgrading from MySQL <8.4 to MySQL >9.0 requires running MySQL 8.4 first:
+>> brew services stop mysql
+>> brew install mysql@8.4
+>> brew services start mysql@8.4
+>> brew services stop mysql@8.4
+>> brew services start mysql
+
+-- To secure it run:
+>> mysql_secure_installation
+
+-- To connect run:
+>> mysql -u root
+
+-- To start mysql now and restart at login:
+>> brew services start mysql
+
+--if you don't want/need a background service you can just run:
+>> /opt/homebrew/opt/mysql/bin/mysqld_safe --datadir\=/opt/homebrew/var/mysql
+
 --to export DB
 
 >> mysqldump -u root -p database_name > export.sql
